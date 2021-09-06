f235ed9313dfc5b7b86b030b6d5b07191929bf34
Shy_Same <olyalychko@gmail.com>
2021-09-06 16:08:07 +0300

    last

--- Hillel_hw_10/settings.py
+++ Hillel_hw_10/settings.py
@@ -61,8 +61,7 @@ if DEBUG:
     INSTALLED_APPS += ['debug_toolbar', ]
     MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware', ]
     INTERNAL_IPS = [
-        '127.0.0.1',
-    ]
+        '127.0.0.1', ]
 
 ROOT_URLCONF = 'Hillel_hw_10.urls'
 
